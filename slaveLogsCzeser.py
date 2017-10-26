308048f83248d53313e6cefe8ac87eb1a6a130f5
sp0f <adam.lyjak@gmail.com>
2017-10-26 10:38:45 +0200

    attach volume

--- slaveLogsCzeser.py
+++ slaveLogsCzeser.py
@@ -19,6 +19,25 @@ def getTag(taggedObject, tagKey):
 def attachVolume(volume,instance):
     print ("Attach "+volume.id+" to instance "+instance.id)
 
+    # build device id list
+    devices = instance.block_device_mappings
+    device_name_list=[]
+    for device in devices:
+        device_name_list.append(device["DeviceName"])
+    # find first free device name (http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html)
+    drive_letter="f"
+    while (("/dev/sd"+drive_letter in device_name_list) or ("/dev/xvd"+drive_letter in device_name_list)):
+        print "Device /dev/sd"+drive_letter + " is occupied"
+        drive_letter=chr(ord(drive_letter)+1)
+    attach_device_name="/dev/sd"+drive_letter
+    print "Attach as "+attach_device_name
+    response=instance.attach_volume(
+        Device=attach_device_name,
+        VolumeId=volume.id,
+        DryRun=False
+    )
+    return response
+
 volumes = ec2.volumes.filter(Filters=[
     {
         'Name': 'tag-key',
